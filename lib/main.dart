65076d7c056d11c20eb4294e48447650d45a11ac
fathrahh <gazoon3@gmail.com>
2022-04-30 04:20:00 +0700

    eps-19

--- lib/main.dart
+++ lib/main.dart
@@ -1,102 +1,59 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 void main() => runApp(MaterialApp(
-      home: MyApp(),
+      home: QuoteList(),
     ));
 
-class MyApp extends StatefulWidget {
+class QuoteList extends StatefulWidget {
+  QuoteList({Key? key}) : super(key: key);
+
   @override
-  _MyAppState createState() => _MyAppState();
+  State<QuoteList> createState() => _QuoteListState();
 }
 
-class _MyAppState extends State<MyApp> {
-  int level = 0;
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [Quote(author: 'chun-li', text: 'muehehhe')];
 
-  void levelUp() {
-    setState(() {
-      level += 1;
-    });
+  Widget quoteTemplate(quote) {
+    return Card(
+        margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(
+                quote.text,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.grey[600],
+                ),
+              ),
+              SizedBox(height: 6.0),
+              Text(
+                quote.author,
+                style: TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.grey[800],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[900],
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text('My First App'),
+        title: Text('Awesome Quotes'),
         centerTitle: true,
-        backgroundColor: Colors.grey[850],
-        elevation: 0.0,
+        backgroundColor: Colors.redAccent,
       ),
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Center(
-                child: CircleAvatar(
-              backgroundImage: AssetImage('assets/avatar.png'),
-              radius: 40.0,
-            )),
-            Divider(
-              height: 60.0,
-              color: Colors.grey[600],
-            ),
-            Text('NAME',
-                style: TextStyle(
-                  color: Colors.grey[400],
-                  letterSpacing: 2.0,
-                )),
-            SizedBox(height: 10.0),
-            Text(
-              'Chun-Li',
-              style: TextStyle(
-                color: Colors.amber,
-                letterSpacing: 1.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Text('CURRENT NINJA LEVEL',
-                style: TextStyle(
-                  color: Colors.grey[400],
-                  letterSpacing: 2.0,
-                )),
-            SizedBox(height: 10.0),
-            Text(
-              '$level',
-              style: TextStyle(
-                color: Colors.amber,
-                letterSpacing: 1.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 30.0),
-            Row(
-              children: <Widget>[
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[400],
-                ),
-                SizedBox(width: 10.0),
-                Text(
-                  'chun.li@thenetninja.co.uk',
-                  style: TextStyle(
-                      color: Colors.grey[400],
-                      letterSpacing: 1.0,
-                      fontSize: 18.0),
-                )
-              ],
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: levelUp,
-        child: Icon(Icons.plus_one),
-        backgroundColor: Colors.grey[800],
+      body: Column(
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       ),
     );
   }
